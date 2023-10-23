13a3192161a39a398f9cd38ef484efd39d21bb41
Matej Hires <m.hires@quadient.com>
2023-10-23 20:29:57 +0200

    Removed ensure created method

--- AutomatedTestingApp/AutomatedTestingApp/Helpers/DataContext.cs
+++ AutomatedTestingApp/AutomatedTestingApp/Helpers/DataContext.cs
@@ -7,7 +7,6 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        Database.EnsureCreated();
     }
     
     public DbSet<User> Users { get; set; }
